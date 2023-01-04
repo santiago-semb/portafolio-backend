b5d32c6080ddc4bf9cbc9ed55ecb68b27bbd14c6
santiago-semb <sansembbb@gmail.com>
2023-01-04 13:54:56 -0300

    act

--- src/main/java/com/example/mbg/WebConfig.java
+++ src/main/java/com/example/mbg/WebConfig.java
@@ -1,11 +1,11 @@
 package com.example.mbg;
 
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
